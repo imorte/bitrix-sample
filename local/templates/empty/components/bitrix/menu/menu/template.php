f7d071651e1080ba745779f476e626756371835b
Kirill P <kirill@webitmaster.ru>
2016-08-28 23:11:37 +0700

    upd

--- local/templates/empty/components/bitrix/menu/menu/template.php
+++ local/templates/empty/components/bitrix/menu/menu/template.php
@@ -15,15 +15,11 @@
 			</div>
 		<div class="collapse navbar-collapse" id="top-nav">
 	<ul class="nav navbar-nav">
-<?
-foreach($arResult as $arItem):
+<?foreach($arResult as $arItem):
 	if($arParams["MAX_LEVEL"] == 1 && $arItem["DEPTH_LEVEL"] > 1) 
-		continue;
-?>
-	<li
-		<?if($arItem["SELECTED"]):?>
-		class="active"<?endif;?>
-	>
+		continue;?>
+
+	<li<?if($arItem["SELECTED"]):?>class="active"<?endif;?>>
 		<a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
 	</li>
 	<?endforeach;?>
